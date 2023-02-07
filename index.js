53e28f2cd9ca044ee1d19548cc6c90467e800216
cuiwenheng <cuiwenheng01@163.com>
2023-02-07 16:18:14 +0800

    运行

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const koa = require('koa')
-const app = new koa()
-
-app.use(function* (ctx) {
-    console.log(`请求参数；${JSON.stringify(this)}`)
-    this.body = `请求进来的参数为：${this.request.url}`
-})
-
-app.listen(30003)
+// const koa = require('koa')
+// const app = new koa()
+//
+// app.use(function* (ctx) {
+//     console.log(`请求参数；${JSON.stringify(this)}`)
+//     this.body = `请求进来的参数为：${this.request.url}`
+// })
+//
+// app.listen(30003)
+console.log('sucsess')
